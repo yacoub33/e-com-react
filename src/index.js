773fd2d135bfd7e61b9d733c4d4202f5c3aa1cef
Yacoub <yacoubhaddad33@outlook.com>
2022-07-31 17:29:39 +0100

    landing + nav

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+} from "@fortawesome/free-solid-svg-icons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
